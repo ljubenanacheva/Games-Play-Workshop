e279346b01ee5a00663a8b692a5859371edad8bd
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-04 19:52:30 +0300

    Create Game context.

---
+++ client/src/contexts/GameContext.js
@@ -0,0 +1,50 @@
+import { useState, createContext, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { gameServiceFactory } from "./services/gameService.js";
+
+export const GameContext = createContext();
+
+export const GameProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [games, setGames] = useState([]);
+
+  const gameService = gameServiceFactory(); //auth.AccessToken
+
+  useEffect(() => {
+    gameService.getAll().then((result) => {
+      setGames(result);
+    });
+  }, []);
+
+  const onCreateGameSubmit = async (data) => {
+    const newGame = await gameService.create(data);
+    setGames((state) => [...state, newGame]);
+    navigate("/catalog");
+  };
+
+  const onGameEditSubmit = async (values) => {
+    const editedGame = await gameService.edit(values._id, values);
+    setGames((state) =>
+      state.map((x) => (x._id === values._id ? editedGame : x))
+    );
+    navigate(`/catalog/${values._id}`);
+  };
+
+  const contextValues = {
+    games,
+    onCreateGameSubmit,
+    onGameEditSubmit,
+  };
+
+  return (
+    <GameContext.Provider value={contextValues}>
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+export const useGameContext = () => {
+  const context = useContext(GameContext);
+  return context;
+};
